1d252901c811b54b3e74a7d1bae6c8a2e82aabe1
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-07-18 20:53:37 +0530

    userlist added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect,Switch} from 'react-router-dom';
-import Users from './user/pages/User' ;
+import Users from './user/pages/Users' ;
 import NewPlace from './places/pages/NewPlace'
 const App = ()  => {
   return( 
--- src/user/pages/User.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Users = () => {
-return <h1>It's works!</h1>;
-};
-
-export default Users;
