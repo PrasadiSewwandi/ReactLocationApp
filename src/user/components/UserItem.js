9f6914d25023e953ad85bd41b394f4c212a3bc4c
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-07-19 10:48:15 +0530

    hover part done

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './UserItem.css'
+import React from 'react';
+import { Link } from 'react-router-dom' ;
+import './UserItem.css';
+import Avatar from '../../shared/components/UIElement/Avatar';
 
 const UserItem = props => {
    return (
        <li className = "user-item">
            <div className = "user-item__content">
+               <Link to = {`/${props.id}/places`}>
                <div className = "user-item__image">
-                   <img src = {props.image} alt = {props.name}/> 
+                   <Avatar image = {props.image} alt = {props.name}/> 
                </div>
                <div className = "user-item__info">
                 <h2>{props.name}</h2>
@@ -14,6 +17,7 @@ const UserItem = props => {
                     {props.placeCount} {props.placeCount ===1 ? 'Place' : 'Places'}
                 </h3>
                </div>
+               </Link>
            </div>
 
        </li>
