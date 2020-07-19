40fd46cd25310dcc5de59586a8aa1c845349ba8d
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-07-20 00:22:10 +0530

    portal added

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './UsersList.css'
 import UserItem from './UserItem';
+import Card from "../../shared/components/UIElement/Card";
+
 
 const UsersList = props => {
     if(props.items.length === 0){
        return ( <div className = "center" >
+          <Card>
            <h2>No users found</h2>
+           </Card>
         </div>
        );
     }
