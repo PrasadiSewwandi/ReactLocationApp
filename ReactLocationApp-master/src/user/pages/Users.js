e17eaf0af9e72e9455239307b849697db79abecf
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-07-24 15:06:46 +0530

    hambooo goda giya aye

---
+++ ReactLocationApp-master/src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Prasadi Sewwandi',
+      image : require('../pictures/a.jpg'),
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
